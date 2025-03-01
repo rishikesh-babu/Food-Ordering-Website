bd4d074a5f29970b743f95b353c25e6a7201c863
rishikesh-babu <rishikeshbabu2004@gmail.com>
2025-03-01 09:03:49 +0530

    Updated Admin footer

--- frontend/src/components/admin/AdminFooter.jsx
+++ frontend/src/components/admin/AdminFooter.jsx
@@ -1,31 +1,48 @@
+import { GitHub, Instagram, LinkedIn, WhatsApp } from '@mui/icons-material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 function AdminFooter() {
     return (
-        <footer className="footer bg-neutral text-neutral-content p-10">
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <Link to={'/'} className="link link-hover">
-                    Home Page 
+        <footer className="footer footer-center bg-neutral text-neutral-content rounded p-10">
+            {/* Navigation Links */}
+            <nav className="grid grid-flow-col gap-4">
+                <Link className="link link-hover">
+                    About Us
+                </Link>
+                <Link className="link link-hover">
+                    Contact
+                </Link>
+                <Link className="link link-hover">
+                    Jobs
+                </Link>
+                <Link to="/" className="link link-hover">
+                    Home page
                 </Link>
             </nav>
+
+            {/* Social Media Links */}
             <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
+                <div className="grid grid-flow-col gap-6">
+                    <a href="https://github.com/rishikesh-babu" target="_blank" rel="noreferrer" className="hover:text-primary hover:scale-105">
+                        <GitHub sx={{ fontSize: 30}} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/rishikesh-babu" target="_blank" rel="noreferrer" className="hover:text-primary hover:scale-105">
+                        <LinkedIn sx={{ fontSize: 30}} />
+                    </a>
+                    <a href="https://www.instagram.com/_ri_s_hi_k.e.sh_/" target="_blank" rel="noreferrer" className="hover:text-primary hover:scale-105">
+                        <Instagram sx={{ fontSize: 30}} />
+                    </a>
+                    <a  target="_blank" rel="noreferrer" className="hover:text-primary transition duration-300">
+                        <WhatsApp sx={{ fontSize: 30}} />
+                    </a>
+                </div>
             </nav>
+
+            {/* Copyright */}
+            <aside>
+                <p>Copyright © {new Date().getFullYear()} - All rights reserved by Food Express 🍔</p>
+            </aside>
         </footer>
     )
 }
